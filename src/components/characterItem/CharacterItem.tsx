0297c3f4bce11cb88512deff985808a9425a23d0
pat <tavelpierre@gmail.com>
2020-02-11 08:34:21 +0100

    changement graph

--- src/components/characterItem/CharacterItem.tsx
+++ src/components/characterItem/CharacterItem.tsx
@@ -38,7 +38,7 @@ const CharacterItem: React.FC<characterProps> = ({character}) => {
           alt=""
         />
       </div>
-      <div style={{textAlign: 'center', margin: "auto"}}>
+      <div style={{textAlign: 'center', margin: "auto", color: 'white', marginTop: 5}}>
         {character.name}
       </div>
     </div>
